b8b5902c278c4224e040bf5dbe8d06e819ebbd1d
Matt Pritchard <matt.pritchard@stfc.ac.uk>
2018-07-02 16:42:16 +0100

    inc to version 0.2 with webfont

--- orgtheme_jasmin/__init__.py
+++ orgtheme_jasmin/__init__.py
@@ -5,4 +5,4 @@ Organisation theme for CEDA Services web components.
 __author__ = "Matt Pritchard"
 __copyright__ = "Copyright 2018 UK Science and Technology Facilities Council"
 
-__version__ = "0.1"
+__version__ = "0.2"
